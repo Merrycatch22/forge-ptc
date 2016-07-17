c1bf33d35b9bfcd6a12366c56559ba36c6668bc4
Nicholas <Nicholas>
2016-07-16 20:04:53 -0700

    sentry ready to deploy

--- a/amod/CrudArmor.java
+++ a/amod/CrudArmor.java
@@ -1,21 +1,28 @@
 package com.a.amod;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.entity.projectile.EntitySnowball;
 import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 public class CrudArmor extends ItemArmor {
 	public float pitch = 15f;
+	public EntityAINearestAttackableTarget.Sorter theNearestAttackableTargetSorter;
+	public boolean isSentry=false;
+	public boolean makeNoise=false;
 
 	public CrudArmor(ArmorMaterial material, int armorType, String name) {
 		super(material, 0, armorType);
@@ -58,15 +65,60 @@ public class CrudArmor extends ItemArmor {
 
 		}
 		if (itemStack.getItem() == ModArmor.crudPlate) {
-			EntityArrow arrow = new EntityArrow(world, player, 5F);
-			arrow.posZ += 1.0 * (Math.random() - 0.5f);
-			arrow.posX += 1.0 * (Math.random() - 0.5f);
-			arrow.posY += 1.0 * (Math.random() - 0.5f);
-			world.spawnEntityInWorld(arrow);
+			List<Entity> list = world.getEntitiesWithinAABBExcludingEntity(
+					player, player.boundingBox.expand(40, 40, 40));
+			List<Entity> listLiving = new ArrayList<Entity>();
+			ListIterator<Entity> iter = list.listIterator();
+
+			theNearestAttackableTargetSorter = new EntityAINearestAttackableTarget.Sorter(player);
+
+			while (iter.hasNext()) {
+				Entity next = iter.next();
+				if (next instanceof EntityLivingBase && !(next instanceof EntityPlayer)) {
+					listLiving.add(next);
+				}
+			}
+			Entity closest;
+			Collections.sort(listLiving, this.theNearestAttackableTargetSorter);
+			if (!listLiving.isEmpty()) {
+				isSentry=true;
+				closest = listLiving.get(0);
+				double d0 = closest.posX - player.posX;
+				/*
+				 * double d1 = next.posY + (double) next.getEyeHeight() -
+				 * 1.100000023841858D - entitysnowball.posY;
+				 */
+				double d1 = closest.posY - player.posY;
+				double d2 = closest.posZ - player.posZ;
+				 double d3 = (double)MathHelper.sqrt_double(d0 * d0 + d2 * d2);
+			        float f2 = (float)(Math.atan2(d2, d0) * 180.0D / Math.PI) - 90.0F;
+			        float f3 = (float)(-(Math.atan2(d1, d3) * 180.0D / Math.PI));
+			        player.rotationPitch = this.updateRotation(player.rotationPitch, f3, 10.f);
+			        player.rotationYaw = this.updateRotation(player.rotationYaw, f2, 20.f);
+			        EntityArrow arrow = new EntityArrow(world, player, 5F);
+					//arrow.posZ += 1.0 * (Math.random() - 0.5f);
+					//arrow.posX += 1.0 * (Math.random() - 0.5f);
+					//arrow.posY += 1.0 * (Math.random() - 0.5f);
+					world.spawnEntityInWorld(arrow);
+					//world.playSoundAtEntity(player, "tile.piston.out", 1.0f, 1.0f);
+			}
+			else{
+				isSentry=false;
+			}
+			if(makeNoise!=isSentry){
+				makeNoise=isSentry;
+				if(makeNoise){
+					world.playSoundAtEntity(player, "mob.chicken.hurt", 1.0f, 1.0f);
+				}
+				else{
+					//world.playSoundAtEntity(player, "mob.ghast.scream", 1.0f, 1.0f);
+				}
+			}
+			
 		}
 		if (itemStack.getItem() == ModArmor.crudLegs) {
 			List<Entity> list = world.getEntitiesWithinAABBExcludingEntity(
-					player, player.boundingBox.expand(40, 40, 40));
+					player, player.boundingBox.expand(10, 10, 10));
 			ListIterator<Entity> iter = list.listIterator();
 			while (iter.hasNext()) {
 				Entity next = iter.next();
@@ -107,8 +159,29 @@ public class CrudArmor extends ItemArmor {
 			// player.capabilities.
 
 			player.capabilities.allowFlying = true;
+			//EntityHoming homing = new EntityHoming(world, player);
+			//homing.posZ += 1.0 * (Math.random() - 0.5f);
+			//homing.posX += 1.0 * (Math.random() - 0.5f);
+			//homing.posY += 1.0 * (Math.random() - 0.5f);
+			//world.spawnEntityInWorld(homing);
 
 		}
 		
 	}
+	private float updateRotation(float p_70663_1_, float p_70663_2_, float p_70663_3_)
+    {
+        float f3 = MathHelper.wrapAngleTo180_float(p_70663_2_ - p_70663_1_);
+
+        if (f3 > p_70663_3_)
+        {
+            f3 = p_70663_3_;
+        }
+
+        if (f3 < -p_70663_3_)
+        {
+            f3 = -p_70663_3_;
+        }
+
+        return p_70663_1_ + f3;
+    }
 }
--- a/amod/EntityHoming.java
+++ a/amod/EntityHoming.java
@@ -8,69 +8,107 @@ import java.util.ListIterator;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.monster.EntityBlaze;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntitySnowball;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
-public class EntityHoming extends EntitySnowball{
+public class EntityHoming extends EntitySnowball {
 	public EntityAINearestAttackableTarget.Sorter theNearestAttackableTargetSorter;
+	public double knockbackStrength;
+
 	public EntityHoming(World world) {
 		super(world);
 		// TODO Auto-generated constructor stub
 	}
-	public EntityHoming(World world, EntityLivingBase entity){
-		super(world,entity);
+
+	public EntityHoming(World world, EntityLivingBase entity) {
+		super(world, entity);
 	}
 
-	public float getGravityVelocity(){
+	public float getGravityVelocity() {
 		return 0.F;
 	}
-	public float func_70182_d(){
+
+	public float func_70182_d() {
 		return 0.6F;
 	}
-	public float func_70183_g(){
+
+	public float func_70183_g() {
 		return 0.F;
 	}
-	public void onUpdate(){
-		
-		List<Entity> list = this.worldObj.getEntitiesWithinAABBExcludingEntity(
-				this.getThrower(), this.boundingBox.expand(20, 20, 20));
+
+	public void onUpdate() {
+
+		List<Entity> list = this.worldObj.getEntitiesWithinAABBExcludingEntity(this.getThrower(),
+				this.boundingBox.expand(20, 20, 20));
 		List<Entity> listLiving = new ArrayList<Entity>();
 		ListIterator<Entity> iter = list.listIterator();
-		
-		theNearestAttackableTargetSorter=new EntityAINearestAttackableTarget.Sorter(this);
-		
-		
+
+		theNearestAttackableTargetSorter = new EntityAINearestAttackableTarget.Sorter(this);
+
 		while (iter.hasNext()) {
-				Entity next = iter.next();
+			Entity next = iter.next();
 			if (next instanceof EntityLivingBase && !(next instanceof EntityPlayer)) {
 				listLiving.add(next);
 			}
 		}
 		Entity closest;
 		Collections.sort(listLiving, this.theNearestAttackableTargetSorter);
-		if(!listLiving.isEmpty()){
-			closest=listLiving.get(0);
+		if (!listLiving.isEmpty()) {
+			closest = listLiving.get(0);
 			double d0 = closest.posX - this.posX;
-			double d1 = closest.posY-this.posY;
+			double d1 = closest.posY - this.posY;
 			double d2 = closest.posZ - this.posZ;
-			double mag=Math.sqrt(d0*d0+d1*d1+d2*d2);
-			double dividePower=5;
-			d0/=mag*dividePower;
-			d1/=mag*dividePower;
-			d2/=mag*dividePower;
-			double prev0=this.motionX;
-			double prev1=this.motionY;
-			double prev2=this.motionZ;
-			double prevMag=Math.sqrt(prev0*prev0+prev1*prev1+prev2*prev2);
-			prev0/=prevMag;
-			prev1/=prevMag;
-			prev2/=prevMag;
-			this.setThrowableHeading(d0+prev0, d1+prev1,
-					d2+prev2, 0.6F, 0.0F);
+			double mag = Math.sqrt(d0 * d0 + d1 * d1 + d2 * d2);
+			double dividePower = 5;
+			d0 /= mag * dividePower;
+			d1 /= mag * dividePower;
+			d2 /= mag * dividePower;
+			double prev0 = this.motionX;
+			double prev1 = this.motionY;
+			double prev2 = this.motionZ;
+			double prevMag = Math.sqrt(prev0 * prev0 + prev1 * prev1 + prev2 * prev2);
+			prev0 /= prevMag;
+			prev1 /= prevMag;
+			prev2 /= prevMag;
+			this.setThrowableHeading(d0 + prev0, d1 + prev1, d2 + prev2, 1.5F, 0.0F);
 		}
 		super.onUpdate();
-		
+
+	}
+
+	public void onImpact(MovingObjectPosition p_70184_1_) {
+		if (p_70184_1_.entityHit != null) {
+			byte b0 = 0;
+
+			if (p_70184_1_.entityHit instanceof EntityBlaze) {
+				b0 = 3;
+			}
+
+			p_70184_1_.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), (float) b0);
+			float f4 = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionZ * this.motionZ);
+
+			if (f4 > 0.0F) {
+				this.knockbackStrength = 0.5;
+				double plusX = this.motionX * (double) this.knockbackStrength * 0.6000000238418579D / (double) f4;
+				double plusZ = this.motionZ * (double) this.knockbackStrength * 0.6000000238418579D / (double) f4;
+
+				p_70184_1_.entityHit.addVelocity(plusX, 0.1D*(1+(double) this.knockbackStrength), plusZ);
+			}
+		}
+
+		for (int i = 0; i < 8; ++i) {
+			this.worldObj.spawnParticle("snowballpoof", this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
+			
+		}
+
+		if (!this.worldObj.isRemote) {
+			this.setDead();
+		}
 	}
 
 }
